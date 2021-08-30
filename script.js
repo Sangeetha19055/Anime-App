c4c34c88966d22526027866e1f7e83c477e4e65e
Sangeetha <89621488+Sangeetha19057@users.noreply.github.com>
2021-08-30 15:32:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ async function getAnime(find) {
         displayAnime(initial_datas.results);
     }
     catch (err) {
-        document.querySelector(".image-conatiner").append("Details Cannot be  Found 😒.");
+        document.querySelector(".image-conatiner").append("Details Cannot be  Found. 😒");
     }
 }
 //-------------------------- displayAnime  ---------------------------
